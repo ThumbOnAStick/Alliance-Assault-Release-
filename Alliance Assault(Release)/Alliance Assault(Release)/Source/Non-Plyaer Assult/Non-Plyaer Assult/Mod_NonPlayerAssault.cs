0d5717198d619582ed3bebf32f5eba66f891e6fa
ThumbStick <120911598+zch0713@users.noreply.github.com>
2024-02-07 21:29:19 +0800

    Factional War Support
    You can observe factional war sites now.

---
+++ Alliance Assault(Release)/Alliance Assault(Release)/Source/Non-Plyaer Assult/Non-Plyaer Assult/Mod_NonPlayerAssault.cs
@@ -0,0 +1,40 @@
+﻿using RimWorld;
+using Verse;
+using HarmonyLib;
+using System.Runtime;
+using UnityEngine;
+
+
+namespace Non_Plyaer_Assault
+{
+    public class NonPlayerAssault : Mod
+    {
+        public NonPlayerAssault(ModContentPack content) : base(content)
+        {
+            harmonyInstance = new Harmony("NonPlayerAssaultRelease");
+            harmonyInstance.PatchAll();
+            settings = base.GetSettings<AllianceAssaultSettings>();
+        }
+
+        public override string SettingsCategory() 
+        {
+            return "AllianceAssaultRelease".Translate();
+        }
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listing_Standard = new Listing_Standard();
+            listing_Standard.Begin(inRect);
+            listing_Standard.Label("AssaultPoints".Translate() + " :" + settings.AssaultPoints, -1f, "AssaultPoints.Explained".Translate());
+            settings.AssaultPoints
+                = (int)listing_Standard.Slider(settings.AssaultPoints, 1000, 5000);
+            //listing_Standard.CheckboxLabeled("ControllableUnits".Translate(),ref settings.ControllableUnits);
+            listing_Standard.End();
+            settings.Write();
+            base.DoSettingsWindowContents(inRect);
+        }
+        public static Harmony harmonyInstance;
+        public static AllianceAssaultSettings settings;
+
+    }
+
+}
---
+++ Alliance Assault(Release)/Alliance Assault(Release)/Source/Non-Plyaer Assult/Non-Plyaer Assult/WorldObjectCompProperties_AllianceAssault.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+using RimWar;
+
+namespace Non_Plyaer_Assault
+{
+    public class WorldObjectCompProperties_AllianceAssult:WorldObjectCompProperties
+    {
+        public WorldObjectCompProperties_AllianceAssult()
+        {
+            compClass = typeof(AllianceAssultComp);
+        }
+    }
+
+    public class AllianceAssultComp : WorldObjectComp
+    {
+        public override IEnumerable<Gizmo> GetGizmos()
+        {
+            base.GetGizmos();
+            MapParent mapParent = parent as MapParent;            
+            if (!mapParent.Faction.HostileTo(Faction.OfPlayer))
+            {
+                yield break;
+            }            
+            if (mapParent.HasMap)
+            {
+                yield return AllianceAssultUtility.CancelAttackCommand(mapParent);
+                yield break;
+            }
+            List<Faction> factions = Find.FactionManager.AllFactions
+                .Where(x => x != Faction.OfPlayer && x.RelationKindWith(Faction.OfPlayer) == FactionRelationKind.Ally&&
+                x.RelationKindWith(mapParent.Faction)==FactionRelationKind.Hostile).ToList();
+            if (factions.Count < 1)
+            {
+                yield break;
+            }
+            yield return AllianceAssultUtility.AllianceAssaultCommand(mapParent);
+        }
+
+        public bool isAllianceAssault;
+        public Faction ally;
+        public override void PostExposeData()
+        {
+            base.PostExposeData();
+            Scribe_Values.Look<bool>(ref isAllianceAssault, "isAllianceAssault", false, false);
+            Scribe_References.Look<Faction>(ref ally, "ally", false);
+        }
+
+        private static readonly string RimWarPackageId = "Torann.RimWar";
+    }
+
+
+}
